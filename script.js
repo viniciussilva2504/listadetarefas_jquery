7924d03de3fb21145995af7dafc60b399b07b4a4
viniciussilva2504 <vinicius.silva2504@gmail.com>
2024-10-02 19:24:56 +0100

    alteração JS e CSS quanto a tipo de dado inserido na caixa de tarefa

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 $(document).ready(function() {
 
+    // Função para escapar tags HTML e evitar inserção de código malicioso
+    function sanitizeInput(input) {
+        return input
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;");
+    }
+
     // Ao submeter o formulário, adiciona uma nova tarefa
     $('#taskForm').submit(function(event) {
         event.preventDefault(); // Previne o comportamento padrão do formulário
         
-        // Captura o valor do input de tarefa
+        // Captura o valor do input de tarefa e sanitiza a entrada
         let task = $('#taskInput').val();
+        let sanitizedTask = sanitizeInput(task); // Escapa o HTML da entrada
         
-        // Adiciona a tarefa à lista
-        if (task) {
-            $('#taskList').append('<li>' + task + '</li>');
+        // Adiciona a tarefa à lista apenas se o campo não estiver vazio
+        if (sanitizedTask) {
+            $('#taskList').append('<li>' + sanitizedTask + '</li>');
             $('#taskInput').val(''); // Limpa o campo de input
         }
     });
