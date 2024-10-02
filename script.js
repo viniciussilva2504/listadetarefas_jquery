ae43d43d78a513e0ede1d5682eec3fcc3bb76ca9
viniciussilva2504 <vinicius.silva2504@gmail.com>
2024-10-02 16:51:16 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+
+    // Ao submeter o formulário, adiciona uma nova tarefa
+    $('#taskForm').submit(function(event) {
+        event.preventDefault(); // Previne o comportamento padrão do formulário
+        
+        // Captura o valor do input de tarefa
+        let task = $('#taskInput').val();
+        
+        // Adiciona a tarefa à lista
+        if (task) {
+            $('#taskList').append('<li>' + task + '</li>');
+            $('#taskInput').val(''); // Limpa o campo de input
+        }
+    });
+    
+    // Marca a tarefa como completa ao clicar
+    $('#taskList').on('click', 'li', function() {
+        $(this).toggleClass('completed');
+    });
+});
